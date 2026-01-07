227963fa3b807f1b5c9d27044c59726f95567352
nareshv-dev <nareshvenkadesh@gmail.com>
2026-01-07 09:21:29 +0530

    Sherlock and anagram

--- anagram2.java
+++ anagram2.java
@@ -1,3 +1,31 @@
+import java.util.*;
+/*  */
 public class anagram2 {
+    public static int sherlockAndAnagrams(String s) {
+    // Write your code here
+    HashMap<String,Integer> map=new HashMap<>();
+    int n=s.length();
     
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<=n;j++){
+            char[] arr=s.substring(i,j).toCharArray();
+            Arrays.sort(arr);
+            String key=new String(arr);
+            map.put(key,map.getOrDefault(key,0)+1);
+        }
+    }
+    int count=0;
+    for(int freq:map.values()){
+        count+= (freq*(freq-1)/2);
+    }
+    return count;
+    
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int result=sherlockAndAnagrams(s);
+        System.out.println(result);
+    }
 }
