a29a5d24e9d545dd2fa95db7d80c2a8dffb60e2e
nareshv-dev <nareshvenkadesh@gmail.com>
2026-01-09 20:20:58 +0530

    Handshake problem

---
+++ handshake.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class handshake {
+    public static void main(String[] args) {
+    /* n --> Number of person
+    At the annual meeting of Board of Directors of Acme Inc. 
+    If everyone attending shakes hands exactly one time with every other attendee, 
+    how many handshakes are there? */
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n * (n - 1) / 2);
+    }
+}
