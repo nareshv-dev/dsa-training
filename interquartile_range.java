68bea0da6fc9d4ed78417b6a561f219133ba2fe8
nareshv-dev <nareshvenkadesh@gmail.com>
2026-01-25 21:47:28 +0530

    DSA Problems

--- interquartile_range.java
+++ interquartile_range.java
@@ -1,13 +1,6 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
 import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
@@ -58,7 +51,7 @@ class Result {
     }
 }
 
-public class Solution {
+public class interquartile_range {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
